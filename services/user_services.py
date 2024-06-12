eff667ccdb1821b4952e0f23951abddcb17932cc
ogzCode <oguzguc44@gmail.com>
2024-06-12 09:36:22 +0300

    refactor: Refactor user services module

--- services/user_services.py
+++ services/user_services.py
@@ -1,53 +1,64 @@
 from sqlalchemy.orm import Session
-from database.models import User
-from database import schemas
+from datetime import datetime
+from models.user import User, UserResponseSchema, UserCreateSchema, UserUpdateSchema
 
 
-def get_user(db: Session, user_id: int):
-    return db.query(User).filter(User.id == user_id).first()
+class UserServices:
+    @staticmethod
+    def get_all_users(db: Session) -> list[UserResponseSchema]:
+        users = db.query(User).all()
+        return [UserResponseSchema(**user.dump()) for user in users]
 
+    @staticmethod
+    def get_user_by_id(db: Session, user_id: int) -> UserResponseSchema:
+        user = db.query(User).filter(User.id == user_id).first()
+        return UserResponseSchema(**user.dump()) if user else None
 
-def get_user_by_username(db: Session, username: str):
-    return db.query(User).filter(User.username == username).first()
+    @staticmethod
+    def create_user(db: Session, user: UserCreateSchema) -> UserResponseSchema:
+        new_user = User(**user)
+        new_user.password = new_user.get_password_hash(new_user.password)
+        db.add(new_user)
+        db.commit()
+        db.refresh(new_user)
+        return UserResponseSchema(**new_user.dump())
 
+    @staticmethod
+    def update_user(db: Session, user_id: int, user: UserUpdateSchema) -> UserResponseSchema:
+        old_user = db.query(User).filter(User.id == user_id).first()
 
-def get_user_by_email(db: Session, email: str):
-    return db.query(User).filter(User.email == email).first()
+        for key, value in user.model_dump().items():
+            if key == "password" and not old_user.verify_password(value):
+                value = old_user.get_password_hash(value)
+                setattr(old_user, key, value)
+            elif key != "password" and value is not None and value != "":
+                setattr(old_user, key, value)
 
+        old_user.updated_at = datetime.now()
 
-def create_user(db: Session, user: schemas.UserCreate):
-    db_user = User(**user.model_dump())
-    db_user.password = db_user.get_password_hash(user.password)
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return db_user
+        db.commit()
+        db.refresh(old_user)
+        
+        return UserResponseSchema(**old_user.dump())
 
+    @staticmethod
+    def delete_user(db: Session, user_id: int) -> bool:
+        db.query(User).filter(User.id == user_id).delete()
+        db.commit()
+        return True
 
-def update_user(id: int, db: Session, user: schemas.UserBase):
-    db_user = get_user(db, id)
-    db_user.username = user.username
-    db_user.email = user.email
-    db.commit()
-    db.refresh(db_user)
-    return db_user
+    @staticmethod
+    def get_user_by_email(db: Session, email: str) -> UserResponseSchema:
+        user = db.query(User).filter(User.email == email).first()
+        return UserResponseSchema(**user.dump()) if user else None
 
+    @staticmethod
+    def user_is_login(db: Session, email: str, password: str) -> UserResponseSchema:
+        user = db.query(User).filter(User.email == email).first()
+        return UserResponseSchema(**user.dump()) if (user and user.verify_password(password)) else None
 
-def delete_user(db: Session, user_id: int):
-    db_user = get_user(db, user_id)
-    db.delete(db_user)
-    db.commit()
-    return db_user
-
-
-def get_users(db: Session):
-    return db.query(User).all()
-
-def get_user_role(db: Session, id: int):
-    return db.query(User).filter(User.id == id).first().role
-
-def delete_all_users(db: Session):
-    db.query(User).delete()
-    db.commit()
-
-
+    @staticmethod
+    def delete_all_users(db: Session) -> bool:
+        db.query(User).delete()
+        db.commit()
+        return True
